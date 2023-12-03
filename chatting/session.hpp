a28ea83d41c1ef88be135672994e52f24899789d
NineHearts <46129253+NineHearts@users.noreply.github.com>
2023-12-03 18:23:26 +0900

    Create session.hpp

---
+++ chatting/session.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <queue>
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+#include <boost/thread/thread.hpp>
+
+namespace asio = boost::asio;
+
+class Server;
+
+class Session : public std::enable_shared_from_this<Session>
+{
+    public:
+        Session(asio::io_service& io_service,
+                 asio::io_service::strand& strand,
+                 Server& server) : 
+                 socket_(io_service), strand_(strand), server_(server){};
+
+        void start();
+
+        asio::ip::tcp::socket& get_socket() { return socket_; }
+
+        void message_receive(const std::string msg);
+    
+    private:
+        void readHandler(const boost::system::error_code& ec);
+        void writeHandler(const boost::system::error_code& ec);
+
+        asio::ip::tcp::socket socket_;
+        asio::io_service::strand& strand_;
+        Server& server_;
+        std::queue<std::string> received_msgs;
+};
